cfccf3f4514eed37eebe7bd642cb63f5a00b0e97
Abigail Eunice San Juan <abigailsanjuan@nyker-macbook-pro.local>
2019-01-17 13:23:38 +0800

    First commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,55 @@
+class ArticlesController < ApplicationController
+
+  # Basic Authenticaation
+  # http_basic_authenticate_with name: "dhh", password: "secret", except: [:index, :show]
+
+  def index
+    # display articles in order
+    @articles = Article.order("title").page(params[:page]).per(10)
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def create
+    #render plain: params[:article].inspect
+    @article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to @article
+      # reuse article_params instead of specifically
+      # identifying which column and value is to be updated
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  private
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
+
+end
---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  get 'welcome/index'
+
+  resources :articles do
+    resources :comments
+  end
+
+  root 'welcome#index'
+
+  get '/articles' => 'articles#index'
+  get '/articles/:id' => 'articles#show'
+  post '/articles/:id' => 'articles#show'
+  delete '/article/destroy/:id' => 'articles#destroy'
+  #delete '/articles/comments/:id' => 'comments#destroy'
+  #delete '/articles/:id' => 'articles#destroy'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
