6881decc5ca6b3a08dd2b4c38af265a2dac763d7
Abigail Eunice San Juan <abigailsanjuan@nyker-macbook-pro.local>
2019-01-18 15:03:29 +0800

    Initial commit'

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,7 @@ class CommentsController < ApplicationController
 
   # Create action
   def create
-    @article = Article.find(params[:article_id])
+    @article = Article.friendly.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
 
     if @comment.save
@@ -17,7 +17,7 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:article_id])
+    @article = Article.friendly.find(params[:article_id])
     @comment = @article.comments.find(params[:id])
     @comment.destroy
     redirect_to article_path(@article)
